d2b54839b93bd060d54302736bd1c0920cf30dfe
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-02-11 02:22:48 +0600

    added common function

--- js/banking.js
+++ js/banking.js
@@ -1,19 +1,27 @@
 
 //using function for deposite
 
-function getInputValue(){
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmountText = depositInput.value;
-    const depositAmount = parseFloat(depositAmountText);
-      //clear input field of deposit
-   depositInput.value = '';
+function getInputValue(inputId){
+    const inputValue = document.getElementById(inputId);
+    const inputAmountText = inputValue.value;
+    const amountValue = parseFloat(inputAmountText);
+      //clear input field of deposit ( 2tatei clear korbe)
+   inputValue.value = ''; 
 
-    return depositAmount;
-
-
+    return amountValue;
 }
 
+// use function for deposite and withdraw filed.......
 
+function updateTotalField(totalFieldId,amount){
+    debugger;
+    const totalElement = document.getElementById(totalFieldId);
+   const totalText = totalElement.innerText; //text hisabe pabo....
+   const previousTotal = parseFloat(totalText); // floaat a convert korbe
+
+   totalElement.innerText = previousTotal + amount;
+
+}
 
 
 
@@ -27,16 +35,18 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
    const depositAmountText = depositInput.value;
    const depositAmount = parseFloat(depositAmountText); */
 
-   const depositAmount = getInputValue();
+   const depositAmount = getInputValue('deposit-input');
 
    //get current Deposit Amount
 
-   const depositTotal = document.getElementById('deposit-total');
-
+   /*
+    const depositTotal = document.getElementById('deposit-total');
    const depositTotalText = depositTotal.innerText; //text hisabe pabo....
    const previousDepositTotal = parseFloat(depositTotalText); // floaat a convert korbe
-
    depositTotal.innerText = previousDepositTotal + depositAmount;
+   
+   */
+  updateTotalField('deposit-total',depositAmount);
 
    //update main Balance ...........
 
@@ -53,17 +63,21 @@ balanceTotal.innerText = previousBalanceTotal + depositAmount;
 // <======handle withdraw balance ======>
 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    const withdrawInput = document.getElementById('withdraw-input');
+    /* const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmountText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText);
+    const withdrawAmount = parseFloat(withdrawAmountText); */
     // console.log(withdrawTotal);
+const withdrawAmount = getInputValue('withdraw-input');
 
     //update withdraw balance........
-
+   /*
    const withdrawTotal =  document.getElementById('withdraw-total');
    const withdrawTotalText = withdrawTotal.innerText;
    const previousWithdrawTotal = parseFloat(withdrawTotalText);
    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+    */
+
+   updateTotalField('withdraw-total',withdrawAmount)
 
 // update main balance after withdraw ............
 
@@ -74,9 +88,7 @@ balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
 
 
 
-//clear withdra field .......
 
-withdrawInput.value = '';
 
 })
 
